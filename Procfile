50d3ef9ee84d41e9ba7051dd493743ba708eaca9
sasha0277 <83279321+sasha0277@users.noreply.github.com>
2022-02-22 18:13:54 +0100

    add Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: npm bakend/server.js
--- backend/app.js
+++ backend/app.js
@@ -38,20 +38,11 @@ app.use('/api/v1',user);
 app.use('/api/v1',order);
 app.use('/api/v1',payment);
 
-// app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-// app.get("*",(req, res) => {
-//     res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
-// })
+app.use(express.static(path.join(__dirname, '../frontend/build')))
+  
+app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname, '../','frontend','build','index.html')))
 // serve Frontend UI
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
-  
-    app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname, '../','frontend','build','index.html')))
-  
-  }else{
-    app.get('/',(req,res)=>res.send('Please set to production'));
-  }
+
 
 
 
