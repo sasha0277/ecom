da34bb8a994374219013797204c1885b713c0277
sasha0277 <83279321+sasha0277@users.noreply.github.com>
2022-02-22 19:20:30 +0100

    changes  Procfiles

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const app = require("./app");
 const cloudinary = require("cloudinary");
 const connectDatabase = require("./config/database");
+const PORT = process.env.PORT ||4000;
 
 // Handling Uncaught Exception
 process.on("uncaughtException", (err) => {
@@ -23,7 +24,7 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const server = app.listen(process.env.PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is working on http://localhost:${process.env.PORT}`);
 });
 
